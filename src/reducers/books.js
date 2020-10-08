6334023b58d32c13e8fbfbeb8002c5221be70cc6
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-08 19:41:46 +0530

    add books reducer

---
+++ src/reducers/books.js
@@ -0,0 +1,26 @@
+const books = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_BOOK':
+      return [
+        ...state,
+        {
+          id: action.id,
+          title: action.title,
+          category: action.category,
+        },
+      ];
+    case 'REMOVE_BOOK':
+      return [
+        ...state,
+        {
+          id: action.id,
+          title: action.title,
+          category: action.category,
+        },
+      ];
+    default:
+      return state;
+  }
+};
+
+export default books;
