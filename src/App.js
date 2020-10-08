59ad3f1d370aaa0e9c79a2ac0d69810cf3fd4e93
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-08 19:19:18 +0530

    add Book component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import BooksList from './components/BooksList';
 
 function App() {
   return (
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,31 +1,26 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from 'react';
+import { connect } from 'react-redux';
 
 function BooksList() {
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <td>Book ID</td>
-                        <td>Title</td>
-                        <td>category</td>
-                    </tr>
-                </thead>
-                <tbody>
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>Book ID</td>
+            <td>Title</td>
+            <td>category</td>
+          </tr>
+        </thead>
+        <tbody />
 
-
-                </tbody>
-
-            </table>
-        </div>
-    )
+      </table>
+    </div>
+  );
 }
 
-const mapStateToProps = state => {
-    return {
-        books: state.books
-    }
-}
+const mapStateToProps = state => ({
+  books: state.books,
+});
 
-export default connect(mapStateToProps)(BooksList)
+export default connect(mapStateToProps)(BooksList);
