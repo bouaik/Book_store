a64f317c9628523347fdbde4c3144b43b92cb853
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-08 15:31:12 +0100

    Initialize the Redux store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+
+const initialState = [
+  {
+    id: Math.floor(Math.random() * 10),
+    title: 'Harry potter',
+    category: 'Fantasy'
+  },
+  {
+    id: Math.floor(Math.random() * 10),
+    title: 'Alchemiste',
+    category: 'Fiction'
+  },
+  {
+    id: Math.floor(Math.random() * 10),
+    title: 'Steve jobs',
+    category: 'Biography'
+  }
+]
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
